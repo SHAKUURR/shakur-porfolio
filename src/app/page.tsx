2df193eaf10105f2d313befa00ae4190e360275f
shakuurr <salmanshakur7@gmail.com>
2025-03-22 22:22:41 +0100

    fixed particle animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
 			<section id="hero">
 				<div className="mx-auto w-full max-w-2xl space-y-8">
 					<div className="gap-2 flex justify-between">
-						<div className="flex-col flex flex-1 space-y-1.5">
+						<div className="flex-col flex flex-1 space-y-1.5 relative overflow-x-clip">
 							<Particles
 								className="absolute inset-0 z-0"
 								quantity={80}
